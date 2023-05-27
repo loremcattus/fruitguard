2e4ee658e098d466a19da50c7c3387c0bd5514a4
MarcoSolis128 <102266890+MarcoSolis128@users.noreply.github.com>
2023-05-26 23:03:22 -0400

    Fixed Blocks

--- src/resources/js/blocks.js
+++ src/resources/js/blocks.js
@@ -1,37 +1,77 @@
 
 import { showMessage } from "./helpers.js";
+
+
+
+
+
+
+
+
 // CREATE
 
 // Obtener referencias a los elementos del formulario 
 
 const formAdd = document.getElementById('addPost');
-const streetsInputAdd = document.getElementById('streetsAdd');
+// const streetsInputAdd = document.getElementById('streetsAdd');
 
 // Evento de envío de formulario 
 
+
 formAdd.addEventListener('submit', async (event) => {
   event.preventDefault(); // Evita el envío de formulario por defecto
+  
+  // ADD INPUTss
+  // let input1Value = document.getElementById("streetsInput1").value;
+  // let input2Value = document.getElementById("streetsInput2").value;
+  // const streets = input1Value + '@' + input2Value;
+  // let inputs = [];
 
+
+  var inputs = document.getElementsByClassName("street-input");
+  // Recorrer los elementos obtenidos
+  let streetsArray = "";
+  let streets = "";
+  for (var i = 0; i < inputs.length; i++) {
+    let inputValue = inputs[i].value.trim();
+    if (i == 0) {
+      streets = inputValue;
+    } else {
+      streets = streets + '@' + inputValue;
+    }
+    streetsArray = streetsArray + `<p class="card-left-side-bottom">${inputValue}</p>`;
+  }
+  // inputs.push(input1Value);
+  // inputs.push(input2Value);
+  // console.log(inputs);
+  // inputs = inputs.join("@");
+  // console.log(typeof inputs, inputs);
+
+
+  // streetsInputAdd = inputs
+  
+  
   // Validar los campos de formulario 
-  if (!streetsInputAdd.value) {
+  if (!streets) {
     showMessage('Por favor, complete todos los campos', 'error');
     return;
   };
-
+  
   try {
+    
     // Obtener los valores de los campos del formulario
-    const streets = streetsInputAdd.value;
-
+    // const streets = streetsInputAdd.value;
+    
     const object = {
       streets
     };
-
+    
     // Obtener el host y el puerto del servidor actual 
-
+    
     const host = window.location.hostname; // localhost
     const port = window.location.port; // 8000
     const pathName = window.location.pathname;// aun no se xd
-
+    
     // Construir la URL base 
     const baseUrl = `http://${host}:${port}`;
 
@@ -66,18 +106,20 @@ formAdd.addEventListener('submit', async (event) => {
       newBlocksElement.insertAdjacentHTML('beforeend', `
               <div class="card-left-side">
                 <p class="card-left-side-top">${data.id}</p>
-                <p class="card-left-side-bottom">${data.streets}</p>
+                ${streetsArray}
               </div>
           `);
 
       // Agregar el nuevo elemento de campaña al contenedor existente
       blocksContainer.prepend(newBlocksElement);
+    } else if (response.status === 409) {
+      throw new Error('La manzana ya ha sido registrada en el foco');
     } else {
       throw new Error('Error al enviar el formulario');
     };
   } catch (error) {
     // Manejar el error 
-    showMessage('Error al enviar el formulario', 'error');
+    showMessage(error, 'error');
     console.log('Error al enviar el formulario:', error);
   };
 });
