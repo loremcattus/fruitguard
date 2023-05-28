478cce109f6558941e253e760b0a9ca6272ece67
loremcatus <102267944+loremcatus@users.noreply.github.com>
2023-05-28 05:16:25 -0400

    Delete user from campaign

--- src/resources/js/single.js
+++ src/resources/js/single.js
@@ -44,4 +44,63 @@ export const singleJS = () => {
     });
   }
 
+  // CAMPAIGN
+
+  // Obtener referencias a los elementos relevantes
+  const removeBtns = document.querySelectorAll('.removeBtn');
+  const modalBackdrop = document.getElementById('modalBackdropRemove');
+  const removeModal = document.getElementById('removeModal');
+  const closeButton = document.querySelector('div.close-modal[data-dismiss="modalRemoveUser"]');
+  const cancelBtn = document.querySelector('#cancelRemoveUser');
+  const confirmBtn = removeModal.querySelector('#confirmRemoveUser');
+
+  // Función para mostrar el modal y el fondo del modal
+  function openRemoveModal() {
+    modalBackdrop.style.display = '';
+    removeModal.style.display = '';
+
+    // Obtener el nombre de usuario
+    const username = this.parentNode.querySelector('.card-left-side-top').textContent;
+    const modalText = removeModal.querySelector('#modal-message');
+    modalText.textContent = `¿Seguro que deseas quitar a ${username} de la campaña?`;
+  }
+
+  // Función para cerrar el modal y el fondo del modal
+  function closeRemoveModal() {
+    modalBackdrop.style.display = 'none';
+    removeModal.style.display = 'none';
+  }
+
+  // Función para manejar el evento de confirmación
+  function handleConfirm() {
+    // Obtener el user-id del elemento padre del botón
+    const userId = this.parentNode.getAttribute('user-id');
+
+    // Realizar el fetch a la URL deseada con el user-id
+    fetch('tu_url', {
+      method: 'DELETE',
+      body: JSON.stringify({ userId }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        // Manejar la respuesta del servidor
+      })
+      .catch(error => {
+        // Manejar el error
+      });
+  }
+
+  // Agregar eventos de clic a los botones de eliminación
+  removeBtns.forEach(btn => {
+    btn.addEventListener('click', openRemoveModal);
+  });
+  
+  // Agregar eventos de clic a los botones de cancelar y confirmar
+  cancelBtn.addEventListener('click', closeRemoveModal);
+  confirmBtn.addEventListener('click', handleConfirm);
+  closeButton.addEventListener('click', closeRemoveModal);
+  modalBackdrop.addEventListener('click', closeRemoveModal);
+
 }
