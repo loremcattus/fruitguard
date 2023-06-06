55798e0ab58f2f2fbd6903f4b9538d5db32d1aff
loremcatus <102267944+loremcatus@users.noreply.github.com>
2023-06-06 00:06:42 -0400

    Modified Add and Update Block. Added more test users. Improved search users to add to campaign

--- src/resources/js/single.js
+++ src/resources/js/single.js
@@ -1,41 +1,44 @@
 export const singleJS = () => {
   const editButton = document.getElementById('edit');
-  const cancelButton = document.getElementById('cancel-edit');
-  const valuePost = document.querySelectorAll('.value-post');
   const buttonsEdit = document.querySelector('.buttons-edit');
 
   if (editButton) {
     editButton.addEventListener('click', function (event) {
+      const valuePost = document.querySelectorAll('.value-post');
       event.preventDefault();
       editButton.style.display = 'none';
       buttonsEdit.style.display = '';
-  
+
       for (let i = 0; i < valuePost.length; i++) {
         const pTag = valuePost[i].querySelector('p');
         const nonPTags = valuePost[i].querySelectorAll(':scope > :not(p)');
-  
+
         if (nonPTags.length > 0) {
-          pTag.style.display = 'none';
+          if (pTag) {
+            pTag.style.display = 'none';
+          }
           for (let j = 0; j < nonPTags.length; j++) {
             nonPTags[j].style.display = '';
           }
         }
       }
     });
-  }
-
-  if (cancelButton) {
+    
+    const cancelButton = document.getElementById('cancel-edit');
     cancelButton.addEventListener('click', function (event) {
+      const valuePost = document.querySelectorAll('.value-post');
       event.preventDefault();
       editButton.style.display = '';
       buttonsEdit.style.display = 'none';
-  
+
       for (let i = 0; i < valuePost.length; i++) {
         const pTag = valuePost[i].querySelector('p');
         const nonPTags = valuePost[i].querySelectorAll(':scope > :not(p)');
-  
+
         if (nonPTags.length > 0) {
-          pTag.style.display = '';
+          if (pTag) {
+            pTag.style.display = '';
+          }
           for (let j = 0; j < nonPTags.length; j++) {
             nonPTags[j].style.display = 'none';
           }
